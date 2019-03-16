50d51ca47b8a8cb282dc9335742fdef507bb5e3e
emdeweier <32056739+emdeweier@users.noreply.github.com>
2019-03-16 07:49:10 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -3,7 +3,7 @@
 	error_reporting(0);
 	session_start();
 
-	mysql_connect("sql306.epizy.com","epiz_22632951","25082016JoVi");
-	mysql_select_db("epiz_22632951_myeffortcalcul");
+	mysql_connect("localhost","root","");
+	mysql_select_db("myeffortcalcul");
 ?>
 </div>
